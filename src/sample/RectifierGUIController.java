93af38dbcb80f765ac1ebe8597322250987907e4
Nathan <nathan.reline@yahoo.com>
2015-02-13 20:08:28 -0700

    Added extension filters for basic image formats, root needs fixing in onBrowseButtonClick() function.

--- src/sample/RectifierGUIController.java
+++ src/sample/RectifierGUIController.java
@@ -11,6 +11,7 @@ import javafx.scene.image.ImageView;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class RectifierGUIController implements Initializable {
@@ -44,17 +45,44 @@ public class RectifierGUIController implements Initializable {
 
     @FXML
     public void onBrowseButtonClick() {
-        File tempFile;
-        //fileChooser.setTitle("Select Image File");
-        tempFile = fileChooser.showOpenDialog(stage);
-        if (tempFile != null) {
-            // is there a better way to check file type to ensure it's an image?
-            if (tempFile.toString().substring(((int)tempFile.length() - 3)) == ".png") {
-                selectedFile = tempFile;
-                filePathTextField.setText(selectedFile.getPath());
-            }
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select an Image File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("PNG File", "*.png"),
+                new FileChooser.ExtensionFilter("JPG File", "*.jpg"),
+                new FileChooser.ExtensionFilter("JPEG File", "*jpeg"));
+
+        File file = fileChooser.showOpenDialog(root.getScene().getWindow()); //showOpenMultipleDialog(root.getScene().getWindow());
+
+        if (file == null) {
+            return;
         }
+
+        filePathTextField.setText(selectedFile.getPath());
+
+        // update view!!!
+
     }
+
+    /* EXAMPLE CODE
+    @FXML public void openJavaFileChooser() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open a Java File");
+		fileChooser.getExtensionFilters().addAll(
+            new ExtensionFilter("Java File", "*.java"));
+		List<File> files = fileChooser.showOpenMultipleDialog(root.getScene().getWindow());
+
+		if(files.isEmpty()) {
+			return;
+		}
+
+		for(File file : files) {
+			addJavaFile(file);
+		}
+
+		updateView(javaFilesView, javaFiles);
+	}
+    */
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
